f123d0121689157d2c3e15de243b35b1a0a58727
ParamUpOne <59622391+ParamUpOne@users.noreply.github.com>
2020-01-24 15:49:32 -0500

    Update CardTrick.java

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
@@ -31,6 +31,7 @@ public class CardTrick {
         //insert code to ask the user for Card value and suit, create their card
         // and search magicHand here
         //Then report the result here
+        //i wasnt able to  code
     }
     
 }
